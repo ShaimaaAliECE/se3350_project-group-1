dbbbae4dd69696560fe8e37bd052c6526c739353
manashav <mvetrive@uwo.ca>
2022-01-25 20:20:27 -0500

    random numbers are generated

--- level2.js
+++ level2.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-
+const numbers = Array.from({length: 10}, () => Math.floor(Math.random() * 20));
 
 app.get('/',(req,res) => {
-    res.send("Welcome to Level 2");
+    res.send('<h1>Welcome to Level 2</h1> '
+    + numbers);
 });
 
 app.listen(3000);
