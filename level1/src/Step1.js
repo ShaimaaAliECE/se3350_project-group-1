33c59c29e5f25de8f8c1202d114c0b5706c619e3
temiiaibana <71097026+temiiaibana@users.noreply.github.com>
2022-01-26 16:28:15 -0500

    Update Step1.js
    editing body

--- level1/src/Step1.js
+++ level1/src/Step1.js
@@ -5,11 +5,14 @@ import {Link} from "react-router-dom";
 const Step1 = () => {
     return (
         <div style="width: 100%;">
-            <div style="width: 60%; height: 100px; float: left;"> 
-            <h3> Step 1/7 </h3> 
+            <div style="width: 10%; height: 100px; float: left;"> 
+                <Link to="" > Back </Link> 
             </div>
-            <div style="margin-left: 90%; height: 100px; "> 
+            <div style="width: 5%; height: 100px; float: right;"> 
                 <Link to="/Step2" > Next </Link>
+            </div>
+            <div style="margin-left: 40%; height: 100px;"> 
+                <h1>Step 1/7</h1>
             </div>
         </div>
     );
