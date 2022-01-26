ef74cf3f3ebeabbaa9c0b0fd8fa9526a1d4e4de5
Alex Salsali <asalsali@uwo.ca>
2022-01-26 15:54:26 -0500

    Created App.js and Step2.js

---
+++ level1/src/Step2.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Step2 = () => {
+
+    return(
+        <div>
+            <h3>
+            Step 2/7
+            </h3>
+            <Link to = "/Step1" >Back</Link>
+        </div>
+    );
+    
+}
