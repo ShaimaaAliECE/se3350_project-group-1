27226e11b0d6e6531f4dfc4f85bd8a88da2db792
Zac <zbennet2@uwo.ca>
2022-01-30 15:36:10 -0500

    Merge sort algorithm checker

--- level1/src/Levels/Level2.js
+++ level1/src/Levels/Level2.js
@@ -2,10 +2,113 @@ import React, { useState } from 'react';
 import ButtonRow from "../components/ButtonRow.js";
 
 function Level2() {
-    const randomNumberArray  = Array.from({length: 10}, () => Math.floor(Math.random() * 20)+ 1);
+  function merge(left, right) {
   
+    let arr = []
+    while (left.length && right.length) {
+        (left[0] < right[0]) ? arr.push(left.shift()) : arr.push(right.shift())
+    }
+    return [...arr, ...left, ...right]
+}
+
+function getSteps(arr) {
+    let arrayLength = arr.length
+    let steps = []
+    let numberOfCycles = Math.ceil(Math.log2(arr.length))
+    let splitArr = []
+    steps.push([...arr])
+    splitArr.push(arr.splice(0, (Math.ceil(arr.length / 2))))
+    splitArr.push(arr)
+    //does the first half of the merge sort (splitting down the array)
+    for (let i = 0; i < numberOfCycles - 1; i++) {
+        arr = splitArr
+        for(let j =0; j <arr.length;j++){
+            steps.push([...arr[j]])
+        }
+        splitArr = []
+        for (let j = 0; j < arr.length; j++) {
+            let subArrayLength = arr[j].length
+            if (subArrayLength == 3) { arr[j].push([]) }
+            let firstHalf = arr[j].splice(0, (Math.ceil(subArrayLength / 2)))
+            splitArr.push(firstHalf)
+            splitArr.push(arr[j])
+        }
+    }
+           let combinedArr = []
+    //combining the split arrays
+    for (let j = 0; j < splitArr.length; j++) {
+        for (let k = 0; k < splitArr[j].length; k++) {
+            (splitArr[j][k] == '') ? combinedArr.push([]) : combinedArr.push([splitArr[j][k]])
+        }
+    }
+  for(let i =0; i <combinedArr.length;i++){
+    if (combinedArr[i]!=''){
+            steps.push([...combinedArr[i]])
+    }
+        }
+    //The second part of merge sort (merging)
+    for (let i = 0; i < numberOfCycles; i++) {
+        let unmergedPairs = []
+        //initializing array
+        for (let j = 0; j < combinedArr.length / 2; j++) { unmergedPairs.push([]) }
+        for (let j = 0; j < combinedArr.length; j++) { unmergedPairs[j >> 1].push(combinedArr[j]) }
+        combinedArr = []
+
+        for (let j = 0; j < unmergedPairs.length; j++) {
+            //console.log(unmergedPairs[j][0], unmergedPairs[j][1])
+            let tmp=[]
+            if (!unmergedPairs[j][0]){
+            let tmp = merge([], unmergedPairs[j][1])
+            }
+            else if (!unmergedPairs[j][1]){
+            let tmp = merge(unmergedPairs[j][0], [])
+            }else{
+                 let tmp = merge(unmergedPairs[j][0], unmergedPairs[j][1])
+            }
+            //console.log(tmp)
+            combinedArr.push(tmp)
+            steps.push([...tmp])
+        }
+    }
+//3D->2D
+    let res =[]
+    for(let i = 0;i<steps.length; i++){
+        res.push([])
+        for(let j = 0; j<steps[i].length;j++){
+            let curr = steps[i][j]
+            
+            if(curr !=''){
+             res[i].push(curr)
+            }
+        }
+    }
+    steps = res
+    res=[]
+    let count = 0
+    let rowNum =0
+    res.push([])
+    //getting the steps formatted into a 3d array of rows
+    for(let i =0; i <steps.length;i++){
+        res[rowNum].push(steps[i])
+        count+=steps[i].length
+        if(count == arrayLength){
+            res.push([])
+            count = 0
+            rowNum+=1
+        }
+    }
+    res.pop()//had to be initialized with an extra space so im removing the left over here
+    return(res)
+}
+    const randomNumberArray  = Array.from({length: 10}, () => Math.floor(Math.random() * 20)+ 1);
+
   const [numbersR1, updateR1]=useState(randomNumberArray)
+
+  const temp = [...numbersR1];
+ console.log(getSteps(temp),"test")
+ 
   const [numbersR2, updateR2]=useState([1,1,1,1,1,1,1,1,1,1])
+
   function rowClick(val,index){
     
     updateR2((numbersR2)=>{
