e1da67155b54e292cdf0cda8b57d1f6c32d6498a
temiiaibana <temiaibana@gmail.com>
2022-01-31 16:15:55 -0500

    working on step 2

--- level1/src/Levels/Step2.js
+++ level1/src/Levels/Step2.js
@@ -5,18 +5,49 @@ import {NavLink} from 'react-router-dom';
 const Step2 = () => { //create main page for step 2
 
     return(
-        <div>
-          <h1>Step 2/7</h1>
-           <p>This is step 2 of the algorithm</p>
-           <NavLink to="/Step1" > Back </NavLink>
-       </div>   
-    );
+        <html>
+            <head></head>
+            <body>  
+                <h1> Step 2/7</h1>
+                <div style="width: 100%;">
+                    <div style="width: 20%; height: 100px; float: left; background: grey;">
+                        &emsp; 1  5  6  9  4 <br></br>
+                        &ensp; &#8601; &emsp; &emsp; &#8600; <br></br>
+                        1  5  6  &emsp; &emsp; 9  4
+                    </div>
+                    <div style="margin-left: 20%; height: 100px; background: grey;">
+                        &emsp; 8  3  7  2  10 <br></br>
+                        &ensp; &#8601; &emsp; &emsp; &#8600; <br></br>
+                        8  3  7  &emsp; 2  10
+                    </div>
+                </div>
+            <p> Step 2: Split new subsets into two more sublists</p>
+            </body>
+        </html>        
+  );
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Step2; //export the constant
 
 /*
+
+<p>  8  3  7  2  10  </p>
+
 <div style="width: 100%;">
             <div style="width: 10%; height: 100px; float: left;"> 
                 <Link to="/Step1" > Back </Link> 
@@ -30,5 +61,27 @@ export default Step2; //export the constant
         </div>
 
 
+
+         <div>
+          <h1>Step 2/7</h1>
+            <body>
+            <div style= "width: 100%;">
+                <div style= "width: 50%; height: 100px; float: left;">
+                    <p>&ensp; 1  5  6  9  4  </p>  
+                    <p> &ensp; &#8601; &emsp; &emsp; &#8600; </p>    
+                    <p>  1  5  6 &emsp; &emsp; 9  4</p>   
+                </div>
+                <div style= "margin-left: 50%; height: 100px;">
+                    <p>&ensp; 8  3  7  2  10  </p>  
+                    <p> &ensp; &#8601; &emsp; &emsp; &#8600; </p>    
+                    <p>  8  3  7 &emsp; &emsp; 2  10</p>   
+                </div>
+            </div>
+            </body>
+           
+           <p>Step 2: Split new subsets into two more sublists</p>
+           <NavLink to="/Step1" > Back </NavLink>
+       </div>   
+    );
 
 */
