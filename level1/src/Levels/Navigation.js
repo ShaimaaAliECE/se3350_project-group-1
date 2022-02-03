249d834afc1bcae2c614c1d76521f7c1f22b5f13
dimamuselmani <68762353+dimamuselmani@users.noreply.github.com>
2022-02-03 11:34:07 -0500

    quit button

--- level1/src/Levels/Navigation.js
+++ level1/src/Levels/Navigation.js
@@ -5,7 +5,11 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {    //create constant that will navigate from page to page
     return (
        <div>
-         <div><h1 class='topRectangle'> &emsp; Home Page</h1></div>
+         <div>
+         <h1 class='topRectangle'> &emsp; Home Page <button class='quitButton'>Quit</button></h1>
+         
+         </div>
+         
       <div>
          <p> &emsp;Click the start button to begin the merge sort algorithm tutorial </p>
          &emsp; <NavLink to="/Step1" class="bttn">Start</NavLink>
