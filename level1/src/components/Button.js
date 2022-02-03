789ef46b52ab1fc164eb5816a9a91eda9971efbd
manashav <mvetrive@uwo.ca>
2022-02-02 22:20:50 -0500

    Added correct and wrong audio to buttons

--- level1/src/components/Button.js
+++ level1/src/components/Button.js
@@ -1,4 +1,6 @@
 import React ,{useState}from 'react';
+import CorrectSound from '../Levels/correct.mp3';
+import WrongSound from '../Levels/wrong.mp3';
 
 function Button(props) {
   
@@ -7,18 +9,22 @@ function Button(props) {
     
   function buttonClick(){
     if(props.nextVal===props.btnId){
+      var correct = new Audio(CorrectSound);
+      correct.play();
       setEnabled(false);
       setClicked(true);
       props.rowClick(props.value);
     }
     else{
+      var wrong = new Audio(WrongSound);
+      wrong.play();
       alert("Wrong - Try Again");
+      
     }
 
-     
       
   }
-  
+ 
   return (
   <button   onClick={buttonClick} disabled={!enabled || !props.enabled} style={{marginRight:props.padding}}>
      {props.value}
