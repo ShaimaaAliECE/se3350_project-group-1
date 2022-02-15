4160cb582409fad9267f2c9c6a4d37aea15e31d7
fokpali <71132881+fokpali@users.noreply.github.com>
2022-02-15 13:26:42 -0500

    added 3 tries before filure

--- project/src/components/Button.js
+++ project/src/components/Button.js
@@ -1,14 +1,33 @@
 import React ,{useState}from 'react';
 import CorrectSound from './correct.mp3';
 import WrongSound from './wrong.mp3';
+import  { Redirect } from 'react-router-dom'
+//do something...
 
+
+let count=0;
 function Button(props) {
-  
+ 
   const [clicked, setClicked]=useState(false);
   const [enabled, setEnabled]=useState(true);
-    
+ 
   function buttonClick(){
-    if(props.nextVal===props.btnId){
+    if (count==2)
+    {
+      alert("Last Try!");
+  
+      return (
+        <button   class={clicked?"bttnCorrect":"bttn2"} onClick={window.location.href = "/LevelsPage"} disabled={!enabled || !props.enabled} style={{marginRight:props.padding}}>
+           {props.value}
+        </button>
+        
+        );
+  
+     
+      
+    }
+
+    else if(props.nextVal===props.btnId){
       var correct = new Audio(CorrectSound);
       correct.play();
       setEnabled(false);
@@ -19,6 +38,7 @@ function Button(props) {
       var wrong = new Audio(WrongSound);
       wrong.play();
       alert("Wrong - Try Again");
+      count++;
       
     }
 
